4de63ef90db88a4393d14ae891775246bc098d79
eoghan <eoghan.firth@gmail.com>
2022-03-06 16:46:52 +0000

    remove cruft from DaoSplitFactory

--- src/DaoSplitFactory.sol
+++ src/DaoSplitFactory.sol
@@ -1,21 +1,9 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.12;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./DaoSplit.sol";
 
 contract DaoSplitFactory {
-    struct Split {
-        address targetToken;
-        address recieveToken;
-        uint256 amount;
-        uint256 ratio;
-        uint256 expires;
-        bool withdrawable;
-    }
-    mapping(address => Split) ActiveSplits;
-    mapping(address => uint256) Distributions;
-
     function RegisterSplit(
         address targetToken,
         uint256 expiry,
