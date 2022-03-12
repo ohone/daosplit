a49e31e5c286ede341a0b86991a5de7a58ef8a0b
eoghan <eoghan.firth@gmail.com>
2022-03-12 14:34:54 +0000

    all tests passing

--- src/test/DaoSplit.t.sol
+++ src/test/DaoSplit.t.sol
@@ -24,13 +24,17 @@ contract DaoSplitTest is DSTest {
         owner = address(this);
     }
 
-    function testCanContributeTargetToken(address user, uint256 amount) public {
+    function testCanContributeTargetToken() public {
+        address user = address(1);
+        uint256 amount = 1337;
+
+        vm.prank(owner);
         targetToken.Mint(user, amount);
 
         vm.startPrank(user);
         targetToken.approve(address(testContract), amount);
-
         testContract.Contribute(user, amount);
+        vm.stopPrank();
 
         // token ownership transferred to contract
         assertEq(targetToken.balanceOf(user), 0);
@@ -98,81 +102,83 @@ contract DaoSplitTest is DSTest {
                 rewards[index]
             );
         }
+        vm.stopPrank();
     }
 
-    function testMinimumMet_RefundContribution_Reverts(
-        address user,
-        uint256 amount
-    ) public {
+    function testMinimumMet_RefundContribution_Reverts() public {
+        // fuzz filter
+        address user = address(1337);
+        uint256 amount = 100;
+
         // populate minimum to ensure completion
         contributeMinimum(owner, targetToken, testContract, minContribution);
+
+        // transfer token allocation to user
         targetToken.Mint(user, amount);
+        vm.startPrank(user);
 
-        vm.prank(user);
+        // user contributes to daosplit
         targetToken.approve(address(testContract), amount);
-
         testContract.Contribute(user, amount);
         assertEq(targetToken.balanceOf(user), 0);
         assertEq(testContract.contributedOf(user), amount);
 
+        // warp to complete split
         vm.warp(expiry + 1);
 
-        vm.expectRevert(bytes(""));
+        // user attempts to get refund
+        vm.expectRevert(bytes("no refunds"));
         testContract.RefundContribution(user);
+
+        vm.stopPrank();
     }
 
-    function testMinimumMet_ClaimsRewards(
-        address user,
-        uint256 amount,
-        uint256[] calldata rewards
-    ) public {
+    function testMinimumMet_ClaimsRewards() public {
+        address user = address(1);
+        uint256 amount = 1337;
+
         // populate minimum to ensure completion
         contributeMinimum(owner, targetToken, testContract, minContribution);
 
+        // mint tokens to user
+        vm.prank(owner);
         targetToken.Mint(user, amount);
 
-        vm.prank(user);
+        vm.startPrank(user);
         targetToken.approve(address(testContract), amount);
-
         testContract.Contribute(user, amount);
         assertEq(targetToken.balanceOf(user), 0);
         assertEq(testContract.contributedOf(user), amount);
-
-        vm.warp(expiry + 1);
+        vm.stopPrank();
 
         // populate rewards
         vm.startPrank(owner);
-        address[] memory rewardsAddresses = new address[](rewards.length);
-        for (uint256 index = 0; index < rewards.length; index++) {
+        uint8 rewardsCount = 5;
+        address[] memory rewardsAddresses = new address[](rewardsCount);
+        for (uint256 index = 0; index < rewardsCount; index++) {
             TestToken token = new TestToken();
-            token.Mint(owner, rewards[index]);
-            token.approve(address(testContract), rewards[index]);
-            testContract.AddReward(owner, address(token), rewards[index]);
+            token.Mint(owner, 1337);
+            token.approve(address(testContract), 1337);
+            testContract.AddReward(owner, address(token), 1337);
+            rewardsAddresses[index] = address(token);
         }
+        vm.stopPrank();
+        vm.warp(expiry + 1);
+
+        // claim all rewards
+        vm.prank(user);
         testContract.ClaimReward(user, rewardsAddresses);
 
-        for (uint256 index = 0; index < rewards.length; index++) {
-            assertEq(
-                TestToken(rewardsAddresses[index]).balanceOf(user),
-                rewards[index]
-            );
+        // assert rewards recieved
+        for (uint256 index = 0; index < rewardsCount; index++) {
+            assertTrue(TestToken(rewardsAddresses[index]).balanceOf(user) > 0);
         }
     }
 
-    function testMinimumMet_multipleContributors_RewardProportionalToContribution(
-        uint16 amount
-    ) public {
-        // populate user addresses
-        address[] memory users = new address[](7);
-        for (uint256 i = 0; i < users.length; i++) {
-            users[i] = address(bytes20(uint160((i + 1) * amount)));
-        }
-
-        // populate rewards amounts from
-        uint256[] memory rewards = new uint256[](10);
-        for (uint256 i = 0; i < rewards.length; i++) {
-            rewards[i] = (i + 1) * amount;
-        }
+    function testMinimumMet_multipleContributors_RewardProportionalToContribution()
+        public
+    {
+        uint16 amount = 37;
 
         // contribute minimum to ensure completion
         uint256 totalContributed = contributeMinimum(
@@ -181,6 +187,18 @@ contract DaoSplitTest is DSTest {
             testContract,
             minContribution
         );
+
+        // populate user addresses
+        address[] memory users = new address[](7);
+        for (uint256 i = 0; i < users.length; i++) {
+            users[i] = address(bytes20(uint160((i + 1) * amount)));
+        }
+
+        // populate rewards amounts
+        uint256[] memory rewards = new uint256[](10);
+        for (uint256 i = 0; i < rewards.length; i++) {
+            rewards[i] = (i + 1) * amount;
+        }
 
         // contribute per-user
         for (uint256 index = 0; index < users.length; index++) {
@@ -242,40 +260,41 @@ contract DaoSplitTest is DSTest {
         }
     }
 
-    function testMinimumMet_ClaimsNonExistingReward_Reverted(
-        address user,
-        uint256 amount,
-        uint256[] calldata rewards
-    ) public {
+    function testMinimumMet_ClaimsNonExistingReward_Reverted() public {
+        address user = address(1);
+        uint256 amount = 1000;
         // populate minimum to ensure completion
         contributeMinimum(owner, targetToken, testContract, minContribution);
 
+        vm.prank(owner);
         targetToken.Mint(user, amount);
 
-        vm.prank(user);
+        vm.startPrank(user);
         targetToken.approve(address(testContract), amount);
-
         testContract.Contribute(user, amount);
+        vm.stopPrank();
         assertEq(targetToken.balanceOf(user), 0);
         assertEq(testContract.contributedOf(user), amount);
 
-        vm.warp(expiry + 1);
-
-        // populate rewards
+        // populate 5 different rewards
         vm.startPrank(owner);
-        for (uint256 index = 0; index < rewards.length; index++) {
+        for (uint256 index = 0; index < 5; index++) {
             TestToken token = new TestToken();
-            token.Mint(owner, rewards[index]);
-            token.approve(address(testContract), rewards[index]);
-            testContract.AddReward(owner, address(token), rewards[index]);
+            token.Mint(owner, 200);
+            token.approve(address(testContract), 200);
+            testContract.AddReward(owner, address(token), 200);
         }
+        vm.stopPrank();
+
+        vm.warp(expiry + 1);
 
         // claim non populated reward
         address[] memory rewardsAddresses = new address[](1);
-        rewardsAddresses[0] = address(1);
-
-        vm.expectRevert(bytes(""));
+        rewardsAddresses[0] = address(1337);
+        vm.startPrank(user);
+        vm.expectRevert(bytes("no rewards for token"));
         testContract.ClaimReward(user, rewardsAddresses);
+        vm.stopPrank();
     }
 
     function contributeMinimum(
